0deee53b0ad586cca756b635d5e3a1f1b04f5b92
Anabelly Passos <anabellybel123@gmail.com>
2024-10-21 15:14:42 -0300

    dados salvos

--- lib/pages/ver_propriedades.dart
+++ lib/pages/ver_propriedades.dart
@@ -1,22 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class VerPropriedades extends StatelessWidget {
-
   const VerPropriedades(String s, {super.key, required String title});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        
-        title: Text(
-          'Minhas Tarefas',
+        title: const Center( 
+          child: Text(
+          'Ver propriedades',
           style: TextStyle(
-            color: Colors.purple[800],
+         color: Colors.brown
+          )
           ),
         ),
       ),
-      body: const Center(
-        
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('propriedades').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.hasError) {
+            return Center(child: Text('Erro: ${snapshot.error}'));
+          }
+
+          // Verifica se existem documentos
+          final propriedades = snapshot.data?.docs;
+          if (propriedades == null || propriedades.isEmpty) {
+            return const Center(child: Text('Nenhuma propriedade encontrada.'));
+          }
+
+          // Exibe a lista de propriedades
+          return ListView.builder(
+            itemCount: propriedades.length,
+            itemBuilder: (context, index) {
+              final propriedade = propriedades[index].data() as Map<String, dynamic>;
+
+              return Card(
+                margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                child: ListTile(
+                  title: Text(propriedade['estado']),
+                  subtitle: Text('Município: ${propriedade['municipio']}'),
+                  trailing: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Lat: ${propriedade['latitude']}'),
+                      Text('Lng: ${propriedade['longitude']}'),
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        },
       ),
     );
   }
